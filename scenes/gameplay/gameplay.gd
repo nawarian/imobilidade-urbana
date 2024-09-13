ac355465b5faab521d042347847ff39fe11d557b
nawarian <nickolas@phpsp.org.br>
2024-09-13 15:14:11 +0200

    chore(ci) fix warnings

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,1 +1,13 @@
 extends Control
+
+@onready var gameover_scene: PackedScene = preload("res://scenes/gameover/gameover.tscn")
+@onready var jumpscare_scene: PackedScene = preload("res://scenes/jumpscare/jumpscare.tscn")
+@onready var outro_scene: PackedScene = preload("res://scenes/outro/outro.tscn")
+
+func _process(delta):
+	if Input.is_action_just_released("ui_left"):
+		SceneManager.change_to(gameover_scene)
+	if Input.is_action_just_released("ui_up"):
+		SceneManager.change_to(jumpscare_scene)
+	if Input.is_action_just_released("ui_right"):
+		SceneManager.change_to(outro_scene)
