7f128d7081d555d0e3f0a70a3e6b5d65ca6ea48c
nawarian <nickolas@phpsp.org.br>
2024-09-13 17:50:48 +0200

    feat(splash) add CamaraDev logo

--- scenes/splash/splash.gd
+++ scenes/splash/splash.gd
@@ -2,10 +2,17 @@ extends Control
 
 @onready var menu_scene: PackedScene = preload("res://scenes/mainmenu/mainmenu.tscn")
 
+@onready var logo: TextureRect = $Logo
 @onready var stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready():
 	stream_player.play()
 	await stream_player.finished
-	await get_tree().create_timer(0.5).timeout
+
+	await FadeInOut.fade_out(logo, 0.5)
+	logo.texture = load("res://scenes/splash/camaradev-logo.png")
+	logo.modulate = Color.WHITE
+	await FadeInOut.fade_in(logo, 1)
+
+	await get_tree().create_timer(1).timeout
 	SceneManager.change_to(menu_scene)
