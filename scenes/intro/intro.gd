d0337ad4b633d1cd291e588ee0728ab903f7c902
nawarian <nickolas@phpsp.org.br>
2024-09-16 22:30:47 +0200

    save progress

--- scenes/intro/intro.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-@onready var gameplay_scene: PackedScene = preload("res://scenes/gameplay/gameplay.tscn")
-
-func _process(delta):
-	if Input.is_action_just_released("ui_accept"):
-		SceneManager.change_to(gameplay_scene)
